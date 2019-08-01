2276770c7e194f7bf5306d0092df7bbc10a75672
Carlos Costa <jr.carlosad@gmail.com>
2019-08-01 19:29:01 -0300

    feat: Avaliação

---
+++ src/layouts/p/[id]/components/Rating/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import SvgLoader from '@components/SvgLoader';
+import { RatingWrapper } from './index.style';
+
+const Rating = ({ rating }: { rating: string }) => {
+  const componentClassName = 'c-rating';
+  const totalRating = 5;
+
+  return (
+    <RatingWrapper>
+      {[...Array(totalRating)].map((item, index) => {
+        const isActive = index + 1 <= Number(rating) ? `active` : `inactive`;
+
+        return (
+          <SvgLoader
+            name="planet"
+            key={index}
+            className={`${componentClassName}__icon ${componentClassName}__icon--${isActive}`}
+          />
+        );
+      })}
+    </RatingWrapper>
+  );
+};
+
+export default Rating;
