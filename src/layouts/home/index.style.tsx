837b61b00bb34fb32f85c7cee60dc7065ca40a68
Carlos Costa <jr.carlosad@gmail.com>
2019-08-01 18:12:08 -0300

    style: Alinhando componentes

--- src/layouts/home/index.style.tsx
+++ src/layouts/home/index.style.tsx
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const HomeBackground = styled.section`
   background-color: var(--background-color);
   border-radius: var(--border-radius);
-  margin: 48px -15px 0;
+  margin: 48px 0 0;
   padding: 35px 15px 45px;
 `;
