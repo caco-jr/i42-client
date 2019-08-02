5e426801f8dbeacb52afc48a6ac7f710b38f59e5
Carlos Costa <jr.carlosad@gmail.com>
2019-08-02 16:59:23 -0300

    feat: Inserindo controle de Interface para props

--- src/layouts/c/[id]/index.tsx
+++ src/layouts/c/[id]/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { ScreenClassProvider, Container } from 'react-grid-system';
+import { AppProps } from '@pages/_app';
 
 import BodyBackground from '@static/styles/BodyBackground';
 import Header from '@components/Header';
 import { CategoryPageWrapper } from './index.style';
 
-interface Props {}
+interface Props extends AppProps {}
 
 const Layout = ({  }: Props) => {
   return (
