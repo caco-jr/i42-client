03321deb95063316898e4cadda33bc0365eda274
Carlos Costa <jr.carlosad@gmail.com>
2019-08-03 13:23:07 -0300

    feat: Pegando posts | Página de Busca

--- src/layouts/busca/components/Posts/index.tsx
+++ src/layouts/busca/components/Posts/index.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
 import { SearchPagePostsWrapper } from './index.style';
+import { PostCardList } from '@components/PostCards/List/index.style';
+import PostCard from '@components/PostCards/Default';
+import PostCardLoading from '@components/PostCards/Default/Loading';
+
+const searchPostsQuery = gql`
+  query searchPosts($term: String!) {
+    searchPosts(term: $term) {
+      title
+      excerpt
+      media {
+        thumbnail
+      }
+      slug
+      categories {
+        slug
+        name
+        color
+      }
+    }
+  }
+`;
 
 interface Props {
   term: string | string[];
@@ -8,7 +32,28 @@ interface Props {
 const SearchPagePosts = ({ term }: Props) => {
   return (
     <SearchPagePostsWrapper>
-      <span>{term}</span>
+      <Query query={searchPostsQuery} variables={{ term }}>
+        {({ loading, data: { searchPosts } }) => (
+          <PostCardList>
+            {!loading
+              ? searchPosts.map((post, index) => {
+                  return (
+                    <PostCard
+                      key={index}
+                      image={post.media.thumbnail}
+                      title={post.title}
+                      content={post.excerpt}
+                      slug={post.slug}
+                      categories={post.categories}
+                    />
+                  );
+                })
+              : [...Array(3)].map((item, index) => (
+                  <PostCardLoading key={index} />
+                ))}
+          </PostCardList>
+        )}
+      </Query>
     </SearchPagePostsWrapper>
   );
 };
