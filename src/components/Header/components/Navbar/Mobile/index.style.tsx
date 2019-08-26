501257b0c33c3abc9271e00d6b52dcba6140cf91
Carlos Costa <jr.carlosad@gmail.com>
2019-08-25 21:10:39 -0300

    feat: Menu Mobile

--- src/components/Header/components/Navbar/Mobile/index.style.tsx
+++ src/components/Header/components/Navbar/Mobile/index.style.tsx
@@ -1,3 +1,36 @@
 import styled from 'styled-components';
 
-export const NavbarMobileWrapper = styled.nav``;
+interface Props {
+  height: number;
+}
+
+export const NavbarMobileWrapper = styled.nav<Props>`
+  background-color: var(--background-color);
+  overflow: hidden;
+  transition: 0.3s all ease;
+  height: 0;
+
+  &.c-navbar-mobile--active {
+    height: ${props => props.height}px;
+  }
+`;
+
+export const NavbarMobileList = styled.section`
+  display: flex;
+  flex-flow: column;
+  padding: 30px 0;
+`;
+
+export const NavbarMobileItem = styled.a`
+  display: inline-flex;
+  color: var(--text-color);
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: bold;
+  padding: 5px;
+  margin: 0 6px;
+
+  &.is-active {
+    font-family: var(--title-font-family);
+  }
+`;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,43 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { Container, Row, Col } from 'react-grid-system';
+import { Container, Row, Col, Hidden, Visible } from 'react-grid-system';
 
 import { HeaderWrapper, HeaderRightColumn } from './header.style';
 import { GlobalStyle } from '@static/styles/GlobalStyle';
 import SvgLoader from '@components/SvgLoader';
 import Search from './components/Search';
-import Navbar from './components/Navbar';
+import NavbarMobile from './components/Navbar/Mobile';
+import NavbarDesktop from './components/Navbar/Desktop';
+import NavbarMobileButton from './components/Navbar/Mobile/Button/index';
 
 const Header = () => {
+  const [isMenuMobileOpen, setIsMenuMobileOpen] = useState(false);
   const componentClassName = 'header-bar';
 
+  const handleMenuMobile = (isOpen: boolean) => {
+    setIsMenuMobileOpen(isOpen);
+  };
+
   return (
-    <HeaderWrapper>
-      <GlobalStyle />
+    <>
+      <Hidden md lg xl>
+        <NavbarMobile isVisible={isMenuMobileOpen} />
+      </Hidden>
 
-      <Container style={{ width: '100%' }}>
-        <Row>
-          <Col sm={2}>
-            <Link href="/">
-              <a className={`${componentClassName}__logo`}>
-                <SvgLoader
-                  name="logo"
-                  className={`${componentClassName}__logo-image`}
-                />
-              </a>
-            </Link>
-          </Col>
+      <HeaderWrapper>
+        <GlobalStyle />
 
-          <Col sm={10}>
-            <HeaderRightColumn>
-              <Navbar />
+        <Container style={{ width: '100%' }}>
+          <Row>
+            <Col xs={10} sm={2}>
+              <Link href="/">
+                <a className={`${componentClassName}__logo`}>
+                  <SvgLoader
+                    name="logo"
+                    className={`${componentClassName}__logo-image`}
+                  />
+                </a>
+              </Link>
+            </Col>
 
-              <Search />
-            </HeaderRightColumn>
-          </Col>
-        </Row>
-      </Container>
-    </HeaderWrapper>
+            <Col xs={2} sm={10}>
+              <HeaderRightColumn>
+                <Visible md lg xl>
+                  <NavbarDesktop />
+
+                  <Search />
+                </Visible>
+
+                <Hidden md lg xl>
+                  <NavbarMobileButton
+                    isOpen={isMenuMobileOpen}
+                    handleMenuMobile={handleMenuMobile}
+                  />
+                </Hidden>
+              </HeaderRightColumn>
+            </Col>
+          </Row>
+        </Container>
+      </HeaderWrapper>
+    </>
   );
 };
 
