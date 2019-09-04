37cbfc9a9e63e833dd434a37fdfb662ee9cb77d7
Carlos Costa <jr.carlosad@gmail.com>
2019-09-04 15:01:42 -0300

    feat: Inserindo link para favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,6 +28,7 @@ export default class MyDocument extends Document<any> {
             content="width=device-width,initial-scale=1,viewport-fit=cover"
           />
           <meta name="description" content="" />
+          <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
           <link
             rel="stylesheet"
             href="https://cdn.rawgit.com/filipelinhares/ress/master/dist/ress.min.css"
