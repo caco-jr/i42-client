e275a1eb52ef27271f5c9b6bedf0023e58c31098
Carlos Costa <jr.carlosad@gmail.com>
2019-09-07 18:58:02 -0300

    feat: Inserindo link de apple-touch-icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,6 +35,29 @@ export default class MyDocument extends Document<any> {
           <link rel="stylesheet" href="/static/styles/FontFamily/style.css" />
           <link rel="manifest" href="/static/manifest/manifest.json" />
           <link rel="manifest" href="manifest.webmanifest" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="48x48"
+            href="/static/manifest/launcher-icon.png?v=1.0.0"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="96x96"
+            href="/static/manifest/launcher-icon@2x.png?v=1.0.0"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="192x192"
+            href="/static/manifest/launcher-icon@4x.png?v=1.0.0"
+          />
+
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+
           <script
             async
             src="https://cdn.jsdelivr.net/npm/pwacompat@2.0.9/pwacompat.min.js"
