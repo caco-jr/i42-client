5bf9607981d7cad6386af817d02254b8cb9dc480
Carlos Costa <jr.carlosad@gmail.com>
2019-09-21 13:40:32 -0300

    feat: Controle de cache para home

--- next.config.js
+++ next.config.js
@@ -24,7 +24,8 @@ const nextConfig = {
     return config;
   },
   target: 'serverless',
-  // transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  transformManifest: manifest =>
+    [{ url: '/', revision: '000001' }].concat(manifest), // add the homepage to the cache
   // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
   // turn on the SW in dev mode so that we can actually test it
   generateInDevMode: false,
