6ba74373d478ac25ac97c51dbecc740a3408f7e0
Carlos Costa <jr.carlosad@gmail.com>
2019-09-21 16:21:52 -0300

    refactor: Teste de SSR

--- next.config.js
+++ next.config.js
@@ -3,27 +3,7 @@ const withOffline = require('next-offline');
 
 const isDev = process.env.NODE_ENV === 'dev';
 
-const nextConfig = {
-  webpack: config => {
-    config.plugins = config.plugins || [];
-
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@pages': path.resolve(__dirname, 'pages/'),
-      '@layouts': path.resolve(__dirname, 'layouts'),
-      '@controllers': path.resolve(__dirname, 'controllers'),
-      '@components': path.resolve(__dirname, 'components'),
-      '@helpers': path.resolve(__dirname, 'helpers/'),
-      '@utils': path.resolve(__dirname, 'utils'),
-      '@interfaces': path.resolve(__dirname, 'interfaces'),
-      '@static': path.resolve(__dirname, 'static'),
-      '@services': path.resolve(__dirname, 'services'),
-      '@lib': path.resolve(__dirname, 'lib')
-    };
-
-    return config;
-  },
-  target: 'serverless',
+const nextOfflineConfig = {
   transformManifest: manifest =>
     [{ url: '/', revision: '000003' }].concat(manifest), // add the homepage to the cache
   // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
@@ -31,9 +11,9 @@ const nextConfig = {
   generateInDevMode: false,
   workboxOpts: {
     swDest: 'static/service-worker.js',
-    // cleanupOutdatedCaches: true,
-    // clientsClaim: true,
-    // skipWaiting: true,
+    cleanupOutdatedCaches: true,
+    clientsClaim: true,
+    skipWaiting: true,
     globPatterns: ['static/**/*'],
     globDirectory: '.',
     runtimeCaching: [
@@ -54,9 +34,34 @@ const nextConfig = {
       }
     ]
   }
+};
+
+const nextConfig = {
+  webpack: config => {
+    config.plugins = config.plugins || [];
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@pages': path.resolve(__dirname, 'pages/'),
+      '@layouts': path.resolve(__dirname, 'layouts'),
+      '@controllers': path.resolve(__dirname, 'controllers'),
+      '@components': path.resolve(__dirname, 'components'),
+      '@helpers': path.resolve(__dirname, 'helpers/'),
+      '@utils': path.resolve(__dirname, 'utils'),
+      '@interfaces': path.resolve(__dirname, 'interfaces'),
+      '@static': path.resolve(__dirname, 'static'),
+      '@services': path.resolve(__dirname, 'services'),
+      '@lib': path.resolve(__dirname, 'lib')
+    };
+
+    return config;
+  },
+  target: 'serverless'
+  // ...nextOfflineConfig
   // onDemandEntries: {
   //   maxInactiveAge: 1000 * 60 * 60 * 24
   // }
 };
 
-module.exports = withOffline(nextConfig);
+// module.exports = withOffline(nextConfig);
+module.exports = nextConfig;
