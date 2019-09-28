7f7e26ff753b28b0242e9ff6043a8b888150ee46
Carlos Costa <jr.carlosad@gmail.com>
2019-09-28 01:19:07 -0300

    feat: Controle de Inicialização

--- components/Scripts/GoogleAdSense/index.tsx
+++ components/Scripts/GoogleAdSense/index.tsx
@@ -2,19 +2,17 @@ import React from 'react';
 
 export const GAdSenseScript = () => {
   return (
-    <>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      ></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-2787359838428098",
-    enable_page_level_ads: true
-  })`
-        }}
-      ></script>
-    </>
+    <script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    ></script>
   );
+};
+
+export const gAdSenseInitialize = () => {
+  // @ts-ignore
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: 'ca-pub-2787359838428098',
+    enable_page_level_ads: true
+  });
 };
