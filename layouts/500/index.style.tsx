ac9df72fc3839a81853ead61080c956c36e53ca4
Carlos Costa <jr.carlosad@gmail.com>
2019-10-15 14:58:10 -0300

    feat: Criando e estilizando página de erro | 500

--- layouts/500/index.style.tsx
+++ layouts/500/index.style.tsx
@@ -1,3 +1,28 @@
 import styled from 'styled-components';
 
-export const ErrorPageWrapper = styled.div``;
+export const ErrorPageWrapper = styled.div`
+  min-height: 100vh;
+  background-color: var(--background-color);
+  display: flex;
+  align-items: center;
+`;
+
+export const ErrorPageBox = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  padding: 20px 30px 60px;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
+  border: solid 1px #979797;
+`;
+
+export const ErrorPageText = styled.p`
+  color: var(--text-color);
+  line-height: 1.33;
+  font-size: 18px;
+  font-weight: 500;
+  max-width: 590px;
+  text-align: center;
+  margin-bottom: 30px;
+`;
--- layouts/500/index.tsx
+++ layouts/500/index.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { ScreenClassProvider, Container } from 'react-grid-system';
-import { ErrorPageWrapper } from './index.style';
+import Link from 'next/link';
+
+import { ErrorPageWrapper, ErrorPageBox, ErrorPageText } from './index.style';
+import Button from '@components/Button';
 
 const ErrorPage = () => {
   return (
     <ScreenClassProvider>
-      <Container>
-        <ErrorPageWrapper>
-          <span>Olá 500</span>
-        </ErrorPageWrapper>
-      </Container>
+      <ErrorPageWrapper>
+        <Container style={{ width: '100%' }}>
+          <ErrorPageBox>
+            <img src="/static/images/background/500.png" />
+
+            <ErrorPageText>
+              Desculpe, encontramos um erro no seu caminho, se precisar de ajuda
+              mande um email para a base em contato@imperio42.com.br.
+            </ErrorPageText>
+
+            <Link href="/">
+              <Button as="a" styleType="outline" href="/">
+                Me leve para a Home
+              </Button>
+            </Link>
+          </ErrorPageBox>
+        </Container>
+      </ErrorPageWrapper>
     </ScreenClassProvider>
   );
 };
