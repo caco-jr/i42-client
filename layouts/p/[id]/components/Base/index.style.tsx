0666b5e4e46726bc7e0b980c751c26fa0f4c3c80
Carlos Costa <jr.carlosad@gmail.com>
2019-10-15 13:32:05 -0300

    style: Ajuste para mobile | Post Page

--- layouts/p/[id]/components/Base/index.style.tsx
+++ layouts/p/[id]/components/Base/index.style.tsx
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 export const PostPageBaseWrapper = styled.section`
   border-radius: var(--border-radius);
   padding: 45px 15px;
+
+  @media (max-width: 574px) {
+    margin: 0 -15px;
+  }
 `;
