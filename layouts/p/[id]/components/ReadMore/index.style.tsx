07af1c07ba40441f7c55fc728aab23c41d869872
Carlos Costa <jr.carlosad@gmail.com>
2019-10-16 13:29:43 -0300

    style: Leia Mais fica fixo ao dar scroll

--- layouts/p/[id]/components/ReadMore/index.style.tsx
+++ layouts/p/[id]/components/ReadMore/index.style.tsx
@@ -4,6 +4,9 @@ import { PostCardList } from '@components/PostCards/List/index.style';
 import { PostCardHorizontalTitle } from '@components/PostCards/Horizontal/index.style';
 
 export const ReadMoreWrapper = styled.section`
+  position: sticky;
+  top: 30px;
+
   ${PostCardList} {
     gap: 30px;
   }
