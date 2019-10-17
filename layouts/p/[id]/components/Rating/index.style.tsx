2137a0ae110b74f0ff5112aac495347d16ec9be1
Carlos Costa <jr.carlosad@gmail.com>
2019-10-17 14:03:07 -0300

    style: Espaçamento do campo de nota

--- layouts/p/[id]/components/Rating/index.style.tsx
+++ layouts/p/[id]/components/Rating/index.style.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const RatingWrapper = styled.section`
-  margin-top: 5px;
+  margin-top: 20px;
 
   .c-rating__icon--inactive .planet-icon__background {
     fill: #313735;
